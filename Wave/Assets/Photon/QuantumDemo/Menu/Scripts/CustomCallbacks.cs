3cdcef09f331c200b1053f3adf32a9cf48bdf24a
ICE <ice.chiruno9@gmail.com>
2024-04-17 16:33:29 +0900

    初期コミット

---
+++ Wave/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class CustomCallbacks : QuantumCallbacks {
+  private void Start()
+  {
+  }
+
+  public override void OnGameStart(Quantum.QuantumGame game) {
+    // paused on Start means waiting for Snapshot
+    if (game.Session.IsPaused) return;
+
+    foreach (var lp in game.GetLocalPlayers()) {
+      Debug.Log("CustomCallbacks - sending player: " + lp);
+      game.SendPlayerData(lp, new Quantum.RuntimePlayer { });
+    }
+  }
+
+  public override void OnGameResync(Quantum.QuantumGame game)
+  {
+    Debug.Log("Detected Resync. Verified tick: " + game.Frames.Verified.Number);
+  }
+}
+
