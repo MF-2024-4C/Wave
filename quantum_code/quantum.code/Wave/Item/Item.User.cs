0e20717db38687331fe984ba31b8e53bd5a64541
Pureria <117608289+Pureria@users.noreply.github.com>
2024-05-17 18:05:01 +0900

    アイテムの作成途中

---
+++ quantum_code/quantum.code/Wave/Item/Item.User.cs
@@ -0,0 +1,10 @@
+﻿namespace Quantum.Wave.Item
+{
+    public unsafe partial struct ItemSys
+    {
+        public static void Interact(Frame f, EntityRef itemEntity, EntityRef playerEntity, ItemSys* item)
+        {
+            //TODO::アイテム作成続き
+        }
+    }
+}
---
+++ quantum_code/quantum.code/Wave/Item/Systems/ItemSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Dynamic;
+
+namespace Quantum.Wave.Item
+{
+    public unsafe class ItemSystem : SystemMainThreadFilter<ItemSystem.Filter>
+    {
+        public struct Filter
+        {
+            public EntityRef Entity;
+            public Transform3D* Transform;
+        }
+
+        public override void Update(Frame f, ref Filter filter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
