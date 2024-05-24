eb1d330edbaf09154ef26453a2602074d4021728
ICE <ice.chiruno9@gmail.com>
2024-05-24 10:12:59 +0900

    イベントシステム、実装完成

---
+++ quantum_code/quantum.code/Events/GameEventSystem.cs
@@ -0,0 +1,27 @@
+﻿namespace Quantum.Events;
+
+public unsafe class GameEventSystem : SystemMainThread, ISignalActivateEvent
+{
+    private GameEventReader _eventReader;
+    private GameEventWriter _eventWriter;
+    public override void OnInit(Frame f)
+    {
+        _eventReader = EventInternal.GetGameEventReader(f);
+        _eventWriter = EventInternal.GetGameEventWriter(f);
+    }
+
+    public void ActivateEvent(Frame f, Trigger trigger)
+    {
+        _eventWriter.Write(new MapEvent { ID = trigger.ID }, f);
+    }
+
+    public override void Update(Frame f)
+    {
+        var ptr = EventInternal.GetSingletonComponent(f).events.container.ptr;
+        f.Heap->Void<GameEventsData>(ptr)->Update(f);
+        foreach (var mapEvent in _eventReader.Read(f))
+        {
+            //Log.Info($"Call Event:{mapEvent.ID}");
+        }
+    }
+}
