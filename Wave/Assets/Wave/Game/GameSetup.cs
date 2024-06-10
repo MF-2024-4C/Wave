bc6b9bf80e080f59f1bba6a449ba354d288d52e4
ICE <ice.chiruno9@gmail.com>
2024-06-11 05:38:22 +0900

    カーソルのロックと非表示を追加

---
+++ Wave/Assets/Wave/Game/GameSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Wave.Game
+{
+    public class GameSetup : MonoBehaviour
+    {
+        private void Awake()
+        {
+            CursorLockedAndHidden();
+        }
+        
+        
+        private static void CursorLockedAndHidden()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
