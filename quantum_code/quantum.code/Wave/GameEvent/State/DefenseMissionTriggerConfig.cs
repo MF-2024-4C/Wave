e7784c7c0d27c8b6e1f7de1f43b4d6124f306c6a
ICE <ice.chiruno9@gmail.com>
2024-07-01 10:47:21 +0900

    ミッションイベントクラスの追加

---
+++ quantum_code/quantum.code/Wave/GameEvent/State/DefenseMissionTriggerConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Quantum.Events;
+
+namespace Quantum;
+
+[Serializable]
+public class DefenseMissionTriggerConfig : BaseItemConfig
+{
+    public Int32 EventID;
+
+    public override void Execute(Frame f, EntityRef item, EntityRef player)
+    {
+        
+    }
+
+    public override void Release(Frame f, EntityRef item, EntityRef player)
+    {
+    }
+}
